17f3f59afb5e334d4063f5fd50151b359758bf66
vkp3012 <vivekpatel301295@gmail.com>
2023-02-01 16:10:36 +0530

    create a movie app using react and port is done and mongodb is connected

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import http from "http"
+import mongoose from "mongoose"
+import "dotenv/config"
+mongoose.set('strictQuery', true)
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+const port = process.env.PORT || 5000
+const server = http.createServer(app);
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("MongoDB Connected");
+    server.listen(port, ()=>{
+        console.log(`server is listening on port ${port}`);
+    });
+}).catch((err)=>{
+    console.log(err);
+    process.exit(1);
+})
