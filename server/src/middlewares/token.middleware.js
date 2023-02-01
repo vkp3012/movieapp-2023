17dddf662cbfb9edd09ce2c3ce261bbe4bfce581
vkp3012 <vivekpatel301295@gmail.com>
2023-02-02 02:32:57 +0530

    token middleware is done

---
+++ server/src/middlewares/token.middleware.js
@@ -0,0 +1,34 @@
+import jsonwebtoken from 'jsonwebtoken';
+import responseHandler from "../handlers/response.handler";
+import userModel from "../models/user.model";
+
+const tokenDecode = (res) => {
+    try {
+        const bearerHeader = req.handers["authorization"];
+        if(bearerHeader){
+            const token = bearerHeader.split(" ")[1];
+            return jsonwebtoken.verify(
+                token,
+                process.env.TOKEN_SECRET
+            );
+        }
+        return false;
+    } catch {
+        return false;
+    }
+};
+
+const auth = async (req,res,next) => {
+    const tokenDecoded = tokenDecode(req);
+
+    if(!tokenDecode) return responseHandler.unauthorize(res);
+
+    const user = await userModel.findById(tokenDecode.data);
+
+    if(!user) return responseHandler.unauthorize(res);
+
+    req.user = user;
+    next();
+};
+
+export default { auth, tokenDecode };
