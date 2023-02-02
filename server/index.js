b3327be3e75ee83864cfc65f33247150c59ac160
vkp3012 <vivekpatel301295@gmail.com>
2023-02-02 12:38:14 +0530

    server part is done or backend is done

--- server/index.js
+++ server/index.js
@@ -4,14 +4,22 @@ import cors from "cors"
 import http from "http"
 import mongoose from "mongoose"
 import "dotenv/config"
+import routes from "./src/routes/index";
 mongoose.set('strictQuery', true)
+
 const app = express();
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
+
+app.use("/api/v1",routes);
+
 const port = process.env.PORT || 5000
+
 const server = http.createServer(app);
+
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("MongoDB Connected");
     server.listen(port, ()=>{
