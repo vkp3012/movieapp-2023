75d8ea7617414122642914168667fd94b72241dc
vkp3012 <vivekpatel301295@gmail.com>
2023-02-02 18:14:03 +0530

    create a public and private api

---
+++ client/src/api/client/public.client.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const baseURL = "https://moonfix-api.vercel.app/app/v1/";
+
+const publicClient = axios.create({
+    baseURL,
+    paramsSerializer : {
+        encode : params => queryString.stringify(params)
+    }
+});
+
+publicClient.interceptors.request.use(async config => {
+    return {
+        ...config,
+        header : {
+            "Content-Type":"application/json"
+        }
+    };
+});
+
+publicClient.interceptors.response.use((response) => {
+    if(response && response.data) return response.data;
+    return response;
+},(err)=>{
+    throw err.response.data;
+})
+
+export default publicClient;
+
