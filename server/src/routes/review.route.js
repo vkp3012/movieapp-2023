835602f273705f23ac3bb9ffc0a2bdc166cc8134
vkp3012 <vivekpatel301295@gmail.com>
2023-02-02 12:24:02 +0530

    review route is done

---
+++ server/src/routes/review.route.js
@@ -0,0 +1,42 @@
+import express from "express";
+import {body} from "express-validator";
+import reviewController from "../controllers/review.controller";
+import rokenMiddleware from "../middlewares/token.middleware"
+import requestHandler from "../handlers/request.handler";
+import tokenMiddleware from "../middlewares/token.middleware";
+
+const router = express.Router({mergeParams : true});
+
+router.get(
+    "/",
+    tokenMiddleware.auth,
+    reviewController.getReviewsOfUser
+);
+
+router.post(
+    "/",
+    tokenMiddleware.auth,
+    body("mediaId")
+        .exists().withMessage("mediaId is required")
+        .isLength({min:8}).withMessage("mediaId can not be empty"),
+    body("content")
+        .exists().withMessage("content is required")
+        .isLength({min:1}).withMessage("content can not be empty"),
+    body("mediaType")
+        .exists().withMessage("mediaType is required")
+        .custom(type => ["movie","tv"].includes(type)).withMessage("mediaType invalid"),
+    body("mediaTitle")
+        .exists().withMessage("mediaTitle is required"),
+    body("mediaPoster")
+        .exists().withMessage("mediaPoster is required"),
+    requestHandler.validate,
+    userController.create
+)
+
+router.delete(
+    "/:reviewId",
+    tokenMiddleware.auth,
+    reviewController.remove
+)
+
+export default router;
