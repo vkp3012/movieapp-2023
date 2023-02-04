b6c7acaefcfdac09ea3b450890746a9f37e6c360
vkp3012 <vivekpatel301295@gmail.com>
2023-02-04 10:16:55 +0530

    favorite utils is done

---
+++ client/src/utils/favorite.utils.js
@@ -0,0 +1,12 @@
+const favoriteUtils = {
+    check : ({
+        listFavorites,
+        mediaId
+    }) =>
+        listFavorites && listFavorites.find(
+            e => 
+                e.mediaId.toString() === mediaId.toString()
+            ) !== undefined
+}
+
+export default favoriteUtils
