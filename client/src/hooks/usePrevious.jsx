0c46a155a5f30e16e6308123fc8855e692656a2d
vkp3012 <vivekpatel301295@gmail.com>
2023-02-04 10:52:05 +0530

    hooks is done

---
+++ client/src/hooks/usePrevious.jsx
@@ -0,0 +1,14 @@
+const { useRef, useEffect } = require("react");
+
+
+const usePrevious = (value) => {
+    const ref = useRef();
+
+    useEffect(()=> {
+        ref.current = value;
+    },[value]);
+
+    return ref.current;
+}
+
+export default usePrevious;
