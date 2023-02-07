1ff9c753c5ab38fd757b08d6ad49132f90ca6ade
vkp3012 <vivekpatel301295@gmail.com>
2023-02-07 11:27:36 +0530

    backdrop slide is done

--- client/src/components/common/BackdropSlide.jsx
+++ client/src/components/common/BackdropSlide.jsx
@@ -1,4 +1,23 @@
 import { Box } from "@mui/material";
 import { SwiperSlide } from "swiper/react";
 import tmdbConfigs from "../../api/configs/tmbd.config";
-import 
+import NavigationSwiper  from "./NavigationSwiper";
+
+const BackdropSlide = ({ backdrops }) => {
+    return (
+        <NavigationSwiper>
+            {[...backdrops].splice(0,10).map((item,index)=>(
+                <SwiperSlide>
+                    <Box sx={{
+                        paddingTop: "60%",
+                        backgroundPosition: "top",
+                        backgroundSize: "cover",
+                        backgroundImage: `url(${tmdbConfigs.backdropPath(item.file_path)})`
+                    }}/>
+                </SwiperSlide>
+            ))}
+        </NavigationSwiper>
+    )
+}
+
+export default BackdropSlide;
