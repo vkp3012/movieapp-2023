c0f4490b1d17ff9e62eab5349f93b0f76a9a0709
vkp3012 <vivekpatel301295@gmail.com>
2023-02-07 10:48:22 +0530

    middlewares error is done

--- server/src/middlewares/token.middleware.js
+++ server/src/middlewares/token.middleware.js
@@ -1,33 +1,37 @@
-import jsonwebtoken from 'jsonwebtoken';
+import jsonwebtoken from "jsonwebtoken";
 import responseHandler from "../handlers/response.handler.js";
 import userModel from "../models/user.model.js";
 
-const tokenDecode = (res) => {
+const tokenDecode = (req) => {
     try {
-        const bearerHeader = req.handers["authorization"];
-        if(bearerHeader){
-            const token = bearerHeader.split(" ")[1];
-            return jsonwebtoken.verify(
-                token,
-                process.env.TOKEN_SECRET
-            );
+        const bearerHeader = req.headers["authorization"];
+
+        if (bearerHeader) {
+        const token = bearerHeader.split(" ")[1];
+
+        return jsonwebtoken.verify(
+            token,
+            process.env.TOKEN_SECRET
+        );
         }
+
         return false;
     } catch {
         return false;
     }
 };
 
-const auth = async (req,res,next) => {
+const auth = async (req, res, next) => {
     const tokenDecoded = tokenDecode(req);
 
-    if(!tokenDecode) return responseHandler.unauthorize(res);
+    if (!tokenDecoded) return responseHandler.unauthorize(res);
 
-    const user = await userModel.findById(tokenDecode.data);
+    const user = await userModel.findById(tokenDecoded.data);
 
-    if(!user) return responseHandler.unauthorize(res);
+    if (!user) return responseHandler.unauthorize(res);
 
     req.user = user;
+
     next();
 };
 
