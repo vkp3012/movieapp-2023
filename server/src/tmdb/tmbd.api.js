15b66b11c009da2ff973bdfb3af2bf500b89eb26
vkp3012 <vivekpatel301295@gmail.com>
2023-02-07 10:50:04 +0530

    tmdb is done

--- server/src/tmdb/tmbd.api.js
+++ server/src/tmdb/tmbd.api.js
@@ -1,36 +1,36 @@
-import axiosClient from "../axios/axios.client.js"
-import tmdbEndpoints from './tmdb.endpoints.js'
+import axiosClient from "../axios/axios.client.js";
+import tmdbEndpoints from "./tmdb.endpoints.js";
 
 const tmdbApi = {
-    mediaList : async ({mediaType,mediaCategory,page}) => await axiosClient.get(
-        tmdbEndpoints.mediaList({mediaType,mediaCategory,page})
+  mediaList: async ({ mediaType, mediaCategory, page }) => await axiosClient.get(
+        tmdbEndpoints.mediaList({ mediaType, mediaCategory, page })
     ),
-    mediaDetalis : async ({mediaType,mediaId}) => await axiosClient.get(
-        tmdbEndpoints.mediaDetail({mediaType,mediaId})
+    mediaDetail: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaDetail({ mediaType, mediaId })
     ),
-    mediaGenres : async ({mediaType}) => await axiosClient.get(
-        tmdbEndpoints.mediaGenres({mediaType})
+    mediaGenres: async ({ mediaType }) => await axiosClient.get(
+        tmdbEndpoints.mediaGenres({ mediaType })
     ),
-    mediaCredits : async ({mediaType,mediaId}) => await axiosClient.get(
-        tmdbEndpoints.mediaCredits({mediaType,mediaId})
+    mediaCredits: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaCredits({ mediaType, mediaId })
     ),
-    mediaVideos : async ({mediaType,mediaId}) => await axiosClient.get(
-        tmdbEndpoints.mediaVideos({mediaType,mediaId})
+    mediaVideos: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaVideos({ mediaType, mediaId })
     ),
-    mediaImage : async ({mediaType,mediaId}) => await axiosClient.get(
-        tmdbEndpoints.mediaImage({mediaType,mediaId})
+    mediaImages: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaImages({ mediaType, mediaId })
     ),
-    mediaRecommend : async ({mediaType,mediaId}) => await axiosClient.get(
-        tmdbEndpoints.mediaRecommend({mediaType,mediaId})
+    mediaRecommend: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaRecommend({ mediaType, mediaId })
     ),
-    mediaSearch : async ({mediaType,query,page}) => await axiosClient.get(
-        tmdbEndpoints.mediaSearch({mediaType,query,page})
+    mediaSearch: async ({ mediaType, query, page }) => await axiosClient.get(
+        tmdbEndpoints.mediaSearch({ mediaType, query, page })
     ),
-    personDetail : async ({personId}) => await axiosClient.get(
-        tmdbEndpoints.personDetail({personId})
+    personDetail: async ({ personId }) => await axiosClient.get(
+        tmdbEndpoints.personDetail({ personId })
     ),
-    personMedias : async ({personId}) => await axiosClient.get(
-        tmdbEndpoints.personMedias({personId})
+    personMedias: async ({ personId }) => await axiosClient.get(
+        tmdbEndpoints.personMedias({ personId })
     )
 };
 
--- server/src/tmdb/tmdb.endpoints.js
+++ server/src/tmdb/tmdb.endpoints.js
@@ -1,36 +1,36 @@
-import tmdbConfig from './tmdb.config.js'
+import tmdbConfig from "./tmdb.config.js";
 
 const tmdbEndpoints = {
-    media : ({mediaType,mediaCategory,page}) => tmdbConfig.getUrl(
-        `$${mediaType}/${mediaCategory}`,{page}
+    mediaList: ({ mediaType, mediaCategory, page }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaCategory}`, { page }
     ),
-    mediaDetail: ({mediaType,mediaId}) => tmdbConfig.getUrl(
+    mediaDetail: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}`
     ),
-    mediaGenres : ({mediaType}) => tmdbConfig.getUrl(
+    mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(
         `genre/${mediaType}/list`
     ),
-    mediaCredits : ({mediaType,mediaId}) => tmdbConfig.getUrl(
+    mediaCredits: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/credits`
     ),
-    mediaVideos : ({mediaType,mediaId}) => tmdbConfig.getUrl(
+    mediaVideos: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/videos`
     ),
-    mediaRecommend : ({mediaType,mediaId}) => tmdbConfig.getUrl(
+    mediaRecommend: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}/recommendations`
     ),
-    mediaImage : ({mediaType,mediaId}) => tmdbConfig.getUrl(
-        `${mediaType}/${mediaId}/videos`
+    mediaImages: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/images`
     ),
-    mediaSearch: ({mediaType,query,page}) => tmdbConfig.getUrl(
-        `search/${mediaType}`,{query,page}
+    mediaSearch: ({ mediaType, query, page }) => tmdbConfig.getUrl(
+        `search/${mediaType}`, { query, page }
     ),
-    personDetail : ({personId}) => tmdbConfig.getUrl(
+    personDetail: ({ personId }) => tmdbConfig.getUrl(
         `person/${personId}`
     ),
-    personMedias : ({personId}) => tmdbConfig.getUrl(
+    personMedias: ({ personId }) => tmdbConfig.getUrl(
         `person/${personId}/combined_credits`
     ),
-}
+};
 
 export default tmdbEndpoints;
